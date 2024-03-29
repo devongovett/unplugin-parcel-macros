318201c592a36c2415beb54becde03b908fb63ad
ambar <ambar.lee@gmail.com>
2024-03-29 23:24:37 +0800

    export type MacroContext

--- unplugin-macros.d.ts
+++ unplugin-macros.d.ts
@@ -1,4 +1,9 @@
 import {UnpluginInstance} from 'unplugin';
 
+export interface MacroContext {
+  addAsset: (asset: {type: string; content: string}) => void;
+  invalidateOnFileChange: (filePath: string) => void;
+}
+
 declare const plugin: UnpluginInstance<void>;
 export = plugin;
